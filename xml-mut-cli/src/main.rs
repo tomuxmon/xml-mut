930c1e9455ce8cd4718ef88d3b5ea9ffc30ff2f9
Tomas Dambrauskas <tomas.sql@protonmail.com>
2023-02-18 07:03:04 +0200

    new xml construction fix

--- xml-mut-cli/src/main.rs
+++ xml-mut-cli/src/main.rs
@@ -6,27 +6,27 @@ fn main() {
     // note: set p@version = p/version@>text should imply where exists p/version
     // since it is required in PredicateEquals.right_side
 
+    let xml = r###"<ItemGroup>
+    <doodles Version="sus"></doodles>
+    <doodles Version="sus"> </doodles>
+    <PackageReference Include="zuzu" Version="sus"> bleep <version>3.0</version> bloop </PackageReference>
+    <PackageReference Include="zuzu" Version="suske"><version>5.0</version> bloop </PackageReference>
+    <PackageReference Include="zizi"> <Version>3.0</Version> </PackageReference>
+    <PackageReference Include="zizibottom"> 
+        <Version>0.0</Version> 
+    </PackageReference>
+    <PackageReference><Version>4</Version></PackageReference>
+    <PackageReference Include="zohan" Version="4.0" />
+</ItemGroup>"###;
+
     let mutation_definition = r###"get ItemGroup/PackageReference as p
 where exists p/version
 set p@version = p/version@>text
 delete p/version"###;
 
-    let xml = r###"<ItemGroup>
-<doodles Version="sus"></doodles>
-<doodles Version="sus"> </doodles>
-<PackageReference Include="zuzu" Version="sus"> bleep <version>3.0</version> bloop </PackageReference>
-<PackageReference Include="zuzu" Version="suske"><version>5.0</version> bloop </PackageReference>
-<PackageReference Include="zizi"> <Version>3.0</Version> </PackageReference>
-<PackageReference Include="zizibottom"> 
-        <Version>0.0</Version> 
-</PackageReference>
-<PackageReference><Version>4</Version></PackageReference>
-<PackageReference Include="zohan" Version="4.0" />
-</ItemGroup>"###;
-
     let (_, mutation) = mutation(mutation_definition).expect("could not parse mutation");
+    let doc: Document = Document::parse(xml).expect("could not parse xml");
 
-    let doc: Document = Document::parse(xml).unwrap();
     let mut replacers: Vec<Replacer> = vec![];
     for node in doc.descendants().filter(|n| n.is_fit(&mutation)) {
         match node.apply(&mutation) {
@@ -42,6 +42,7 @@ delete p/version"###;
         new_xml.push_str(&replacer.replacement);
         offset = replacer.bounds.end;
     }
+    new_xml.push_str(&doc.input_text()[offset..doc.input_text().len()]);
 
     println!("{new_xml}");
 }
