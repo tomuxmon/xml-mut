2e63cf5bb20cb0aec9efc7df0a7c62a3e8a97cca
Tomas Dambrauskas <tomas.sql@protonmail.com>
2023-03-11 13:53:22 +0200

    some more renamings.

--- xml-mut-impl/src/mutable.rs
+++ xml-mut-impl/src/mutable.rs
@@ -28,8 +28,8 @@ impl<'a, 'input: 'a> Mutable for Node<'a, 'input> {
 
     fn get_replacers(&self, mutation: &Mutation) -> Vec<Replacer> {
         let mut replacers: Vec<Replacer> = vec![];
-        if let Some(set_op) = mutation.set_clause.clone() {
-            for ref assignment in set_op.assignments.into_iter() {
+        if let Some(set_clause) = mutation.set_clause.clone() {
+            for ref assignment in set_clause.assignments.into_iter() {
                 // TODO: replacers should be inside node bounds
                 // TODO: validate replacer.bounds.is_empty()
                 match self.assign(assignment) {
@@ -38,12 +38,12 @@ impl<'a, 'input: 'a> Mutable for Node<'a, 'input> {
                 }
             }
         }
-        if let Some(ref delete_statement) = mutation.delete_clause.clone() {
+        if let Some(ref delete_clause) = mutation.delete_clause.clone() {
             // TODO: count the number of replacers on top of this node and
             // when children is empty spawn another replacer triming end tag.
             // TODO: replacers should be inside node bounds
             // TODO: validate replacer.bounds.is_empty()
-            match self.delete(&delete_statement.node_path) {
+            match self.delete(&delete_clause.node_path) {
                 Ok(replacer) => replacers.push(replacer),
                 Err(error) => println!("{error:?}"), // TODO: better error handling, collect maybe?
             }
