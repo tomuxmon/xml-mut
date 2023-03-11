45cb7052431020c515a74ac16790361f03831be9
Tomas Dambrauskas <tomas.sql@protonmail.com>
2023-03-11 18:03:17 +0200

    removed useless  comments

--- xml-mut-cli/src/main.rs
+++ xml-mut-cli/src/main.rs
@@ -6,45 +6,8 @@ use xml_mut_impl::prelude::*;
 use xml_mut_parse::prelude::*;
 
 fn main() {
-    // note: set p@version = p/version@>text should imply where exists p/version
-    // since it is required in PredicateEquals.right_side
-
-    //     let xml = r###"<ItemGroup>
-    //     <doodles Version="sus"></doodles>
-    //     <doodles Version="sus"> </doodles>
-    //     <PackageReference Include="zuzu" Version="sus"> bleep <version>3.0</version> bloop </PackageReference>
-    //     <PackageReference Include="zuzu" Version="suske"><version>5.0</version> bloop </PackageReference>
-    //     <PackageReference Include="zizi"> <Version>3.0</Version> </PackageReference>
-    //     <PackageReference Include="zizibottom">
-    //         <Version>0.0</Version>
-    //     </PackageReference>
-    //     <PackageReference><Version>4</Version></PackageReference>
-    //     <PackageReference Include="zohan" Version="4.0" />
-    // </ItemGroup>"###;
-
-    //     let mutation_definition = r###"get ItemGroup/PackageReference as p
-    // where exists p/version
-    // set p@version = p/version@>text
-    // delete p/version"###;
-
-    // let (_, mutation) = mutation_surounded_multispace0(mutation_definition).expect("could not parse mutation");
-    // let doc: Document = Document::parse(xml).expect("could not parse xml");
-
-    // TODO: define how mullation is passed to the CLI
-    // 1. isinlined as cli args
-    // 2. a path to a mutation definition file
-    // 3. a name of mutation defination file in a common folder ~/.xml-mut/
-    // 4. can one apply multiple mutations at once with a single call?
-
-    // TODO: define how xml path is passed to the CLI
-    // 1. a path to a single xml file
-    // 2. a path to a folder of xml files
-    // 3. shoul it drill down in to sub folders? (CLI arg for that)
-
-    // relative path
-    // absolute path
-    // multiple paths
-    // single path pattern
+    // TODO: a name of mutation defination file in a common folder ~/.xml-mut/
+    // TODO: shoul it drill down in to sub folders? (CLI arg for that)
 
     // TODO: in addition to xml paths
     // folder path with desired xml extensions
